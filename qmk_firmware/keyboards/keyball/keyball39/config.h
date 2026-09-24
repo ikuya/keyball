e2753cc27917ec8a53d2c6cb8a01e534d7635333
Ikuya Fukumoto <ikuya.fukumoto@gmail.com>
2026-09-24 15:55:37 +0900

    Keyball39にも44と同等の改造を加えた。

--- qmk_firmware/keyboards/keyball/keyball39/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/config.h
@@ -77,3 +77,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // To squeeze firmware size
 #undef LOCKING_SUPPORT_ENABLE
 #undef LOCKING_RESYNC_ENABLE
+
+// AUTO MOUSE LAYERのデフォルトTIMEOUT値
+#define AUTO_MOUSE_TIME 60000
